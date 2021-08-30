62fad7591f85b02a3ff3716a4ee1a8437ae30c86
tsvetelin@hackpartners.com <tsvetelin@hackpartners.com>
2021-08-30 19:32:03 +0300

    Update libraries; Setup view binding

--- app/src/main/java/com/example/weatherbylocation/ui/activities/main/MainActivity.kt
+++ app/src/main/java/com/example/weatherbylocation/ui/activities/main/MainActivity.kt
@@ -2,14 +2,18 @@ package com.example.weatherbylocation.ui.activities.main
 
 import android.app.Activity
 import android.os.Bundle
+import com.example.weatherbylocation.databinding.ActivityMainBinding
 
 class MainActivity : Activity(), MainActivityView {
 
     private val presenter = MainActivityPresenter(this)
 
+    private lateinit var viewBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
     }
 
 }
